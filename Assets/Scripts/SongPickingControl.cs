fd6fe76ae60292c1e446d45a59a9af7ccc78077b
Baturay Palas <baturay@outlook.fr>
2020-12-07 11:42:03 +0300

    not working upstream

--- Assets/Scripts/SongPickingControl.cs
+++ Assets/Scripts/SongPickingControl.cs
@@ -19,9 +19,6 @@ public class SongPickingControl : MonoBehaviour
 	//the messenger to pass through other scenes
 	public GameObject songInfoMessengerPrefab;
 
-	//animation
-	private RotationAnimation flipInAnimation;
-	private RotationAnimation flipOutAnimation;
 	private const float FlipAnimationDuration = 0.25f;
 
 	//rotation
@@ -34,10 +31,6 @@ public class SongPickingControl : MonoBehaviour
 
 	void Awake()
 	{
-		//init animation
-		flipInAnimation = new RotationAnimation();
-		flipOutAnimation = new RotationAnimation();
-
 		//check if song messenger exists (if player returned from other scenes)
 		if (SongInfoMessenger.Instance != null)
 		{
@@ -50,124 +43,18 @@ public class SongPickingControl : MonoBehaviour
 		}
 	}
 
-	void Start()
+	//menu flip animations
+    IEnumerator FlipAnimCoroutine(RectTransform rectT, Vector3 start, Vector3 end, float duration, Vector2 originalPivot, Vector2 animationPivot)
 	{
-		//LogicScript.OpenSongBoardEvent += FlipToSongFromFirst;
-	}
-
-    void OnDestroy()
-    {
-		//LogicScript.OpenSongBoardEvent -= FlipToSongFromFirst;
-	}
-
-
-    /*menu transition and animations
-	they can be simplified !*/
-    public void FlipToFirstFromSong()
-    {
-        StartCoroutine(flipOutAnimation.AnimationCoroutine(
-            secondBoardTransform,
-            Vector3.zero,
-            verticalFlip,
-            FlipAnimationDuration,
-            secondBoardTransform.pivot,
-            bottomEdgePivot
-        ));
-        StartCoroutine(flipInAnimation.AnimationCoroutine(
-            firstBoardTransform,
-            verticalFlip,
-            Vector3.zero,
-            FlipAnimationDuration,
-            firstBoardTransform.pivot,
-            topEdgePivot
-        ));
-		secondBoardIsActive = false;
-    }
-
-    public void FlipToSongFromFirst()
-    {
-        StartCoroutine(flipOutAnimation.AnimationCoroutine(
-            firstBoardTransform,
-            Vector3.zero,
-            verticalFlip,
-            FlipAnimationDuration,
-            firstBoardTransform.pivot,
-            topEdgePivot
-        ));
-        StartCoroutine(flipInAnimation.AnimationCoroutine(
-            secondBoardTransform,
-            verticalFlip,
-            Vector3.zero,
-            FlipAnimationDuration,
-            secondBoardTransform.pivot,
-            bottomEdgePivot
-        ));
-		secondBoardIsActive = true;
-    }
-
-    public void SettingsButtonToggle()
-	{
-		if (secondBoardIsActive)
-        {
-			FlipToFirstFromSong();
-        }
-		else if (settingsIsActive)
+		rectT.pivot = animationPivot;
+		float i = 0f;
+		while (i <= 1f)
 		{
-			FlipToFirstFromSettings();
+			i += Time.deltaTime / duration;
+			rectT.transform.localRotation = Quaternion.Euler(Vector3.Lerp(start, end, i));
+			yield return null;
 		}
-		else
-		{
-			FlipToSettingsFromFirst();
-		}
-	}
-
-	public void FlipToSettingsFromFirst()
-	{
-		StartCoroutine(flipOutAnimation.AnimationCoroutine(
-			firstBoardTransform,
-			Vector3.zero,
-			horizontalFlip,
-			FlipAnimationDuration,
-			firstBoardTransform.pivot,
-			topEdgePivot
-		));
-		StartCoroutine(flipInAnimation.AnimationCoroutine(
-			settingsBoardTransform,
-			horizontalFlip,
-			Vector3.zero,
-			FlipAnimationDuration,
-			settingsBoardTransform.pivot,
-			bottomEdgePivot
-		));
-		settingsIsActive = true;
-		aboutLayer.SetActive(false);
-		settingsLayer.SetActive(true);
-	}
-
-	public void FlipToFirstFromSettings()
-	{
-		StartCoroutine(flipOutAnimation.AnimationCoroutine(
-			settingsBoardTransform,
-			Vector3.zero,
-			horizontalFlip,
-			FlipAnimationDuration,
-			settingsBoardTransform.pivot,
-			bottomEdgePivot
-		));
-		StartCoroutine(flipInAnimation.AnimationCoroutine(
-			firstBoardTransform,
-			horizontalFlip,
-			Vector3.zero,
-			FlipAnimationDuration,
-			firstBoardTransform.pivot,
-			topEdgePivot
-		));
-		settingsIsActive = false;
-	}
-
-	public void AboutToggle()
-	{
-		StartCoroutine(AboutAnim());
+		rectT.pivot = originalPivot;
 	}
 
 	IEnumerator AboutAnim()
@@ -182,5 +69,47 @@ public class SongPickingControl : MonoBehaviour
 			yield return null;
 		}
 		aboutLayer.transform.localScale = new Vector3(1, 1, 1);
+	}
+
+    public void FlipToFirstFromSong()
+    {
+        StartCoroutine(FlipAnimCoroutine( secondBoardTransform, Vector3.zero, verticalFlip, FlipAnimationDuration, secondBoardTransform.pivot, bottomEdgePivot));
+        StartCoroutine(FlipAnimCoroutine( firstBoardTransform, verticalFlip, Vector3.zero, FlipAnimationDuration, firstBoardTransform.pivot, topEdgePivot));
+		secondBoardIsActive = false;
+    }
+
+    public void FlipToSongFromFirst()
+    {
+        StartCoroutine(FlipAnimCoroutine( firstBoardTransform, Vector3.zero, verticalFlip, FlipAnimationDuration, firstBoardTransform.pivot, topEdgePivot));
+        StartCoroutine(FlipAnimCoroutine( secondBoardTransform, verticalFlip, Vector3.zero, FlipAnimationDuration, secondBoardTransform.pivot, bottomEdgePivot));
+		secondBoardIsActive = true;
+    }
+
+    public void SettingsButtonToggle()
+	{
+		if (secondBoardIsActive) FlipToFirstFromSong();
+		else if (settingsIsActive) FlipToFirstFromSettings();
+		else FlipToSettingsFromFirst();
+	}
+
+	public void FlipToSettingsFromFirst()
+	{
+		StartCoroutine(FlipAnimCoroutine( firstBoardTransform, Vector3.zero, horizontalFlip, FlipAnimationDuration, firstBoardTransform.pivot, topEdgePivot));
+		StartCoroutine(FlipAnimCoroutine( settingsBoardTransform, horizontalFlip, Vector3.zero, FlipAnimationDuration, settingsBoardTransform.pivot, bottomEdgePivot));
+		settingsIsActive = true;
+		aboutLayer.SetActive(false);
+		settingsLayer.SetActive(true);
+	}
+
+	public void FlipToFirstFromSettings()
+	{
+		StartCoroutine(FlipAnimCoroutine( settingsBoardTransform, Vector3.zero, horizontalFlip, FlipAnimationDuration, settingsBoardTransform.pivot, bottomEdgePivot));
+		StartCoroutine(FlipAnimCoroutine( firstBoardTransform, horizontalFlip, Vector3.zero, FlipAnimationDuration, firstBoardTransform.pivot, topEdgePivot));
+		settingsIsActive = false;
+	}
+
+	public void AboutToggle()
+	{
+		StartCoroutine(AboutAnim());
 	}
 }
