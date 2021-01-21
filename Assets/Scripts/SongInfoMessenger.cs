f003d5eb93ebe03a8de05158ea8d7b1335d0d6df
Baturay Palas <baturay@outlook.fr>
2021-01-21 11:45:58 +0300

    removed tmp

--- Assets/Scripts/SongInfoMessenger.cs
+++ Assets/Scripts/SongInfoMessenger.cs
@@ -8,7 +8,6 @@ public class SongInfoMessenger : MonoBehaviour
     public static SongInfoMessenger Instance = null;
 
     [NonSerialized] public SongInfo currentSong;
-    [NonSerialized] public SongCollection currentCollection;
     [NonSerialized] public int currSongNumber;
     [NonSerialized] public int currCollNumber;
 
